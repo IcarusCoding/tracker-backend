5addd122be9e94a79dfbb000d76c23730e2d5d1f
IcarusCoding <deniz.groenhoff@uni-oldenburg.de>
2024-09-03 17:11:50 +0200

    Updated generic router and CRUD

--- utils/crud.py
+++ utils/crud.py
@@ -30,7 +30,10 @@ class GenericCRUD(Generic[T]):
         obj = self.session.get(self.model, obj_id.id)
         if obj is None:
             return None
-        obj = self.model(**kwargs)
+
+        for key, value in kwargs.items():
+            setattr(obj, key, value)
+
         self.session.add(obj)
         self.session.commit()
         self.session.refresh(obj)
@@ -41,3 +44,11 @@ class GenericCRUD(Generic[T]):
         if obj:
             self.session.delete(obj)
             self.session.commit()
+
+
+class UniqueNameCRUD(GenericCRUD[T], Generic[T]):
+    def __init__(self, model: Type[T], session: Session):
+        super().__init__(model, session)
+
+    def read_by_name(self, name: str) -> Optional[T]:
+        return self.session.exec(select(self.model).where(self.model.name == name)).first()
--- utils/router.py
+++ utils/router.py
@@ -1,40 +1,45 @@
-from typing import Generic, Type
+from dataclasses import dataclass
+from typing import Generic, Type, Optional
 
 from fastapi import APIRouter, status, Depends, HTTPException
-from sqlmodel import Session, SQLModel
-from typing_extensions import TypeVar
+from sqlmodel import SQLModel
+from typing_extensions import TypeVar, override
 
 from controller.auth import ScopeValidator
 from deps import get_session
-from utils.crud import GenericCRUD
+from utils.crud import GenericCRUD, UniqueNameCRUD
 from utils.models import ObjectIdentifier
 
-Model = TypeVar("Model", bound=SQLModel)
-Create = TypeVar("Create", bound=SQLModel)
-Read = TypeVar("Read", bound=SQLModel)
+Model = TypeVar("Model", bound=Optional[SQLModel])
+Create = TypeVar("Create", bound=Optional[SQLModel])
+Read = TypeVar("Read", bound=Optional[SQLModel])
+Update = TypeVar("Update", bound=Optional[SQLModel])
+Delete = TypeVar("Delete", bound=Optional[ObjectIdentifier])
 CRUD = TypeVar("CRUD", bound=GenericCRUD)
 
+UCRUD = TypeVar("UCRUD", bound=UniqueNameCRUD)
 
-class GenericRouter(APIRouter, Generic[Model, Create, Read, CRUD]):
+
+@dataclass
+class Models:
+    base: Type[Model]
+    create: Type[Create] = None
+    read: Type[Read] = None
+    update: Type[Update] = None
+    delete: Type[Delete] = ObjectIdentifier
+    crud: Type[CRUD] = GenericCRUD
+
+
+class GenericRouter(APIRouter, Generic[Model, Create, Read, Update, Delete, CRUD]):
 
     # TODO decorator
-    def __init__(self, model: Type[Model],
-                 read_model: Type[Read],
-                 create_model: Type[Create],
-                 crud_model: Type[CRUD],
-                 tag: str,
-                 # TODO proper generics
-                 crud: CRUD = None,
-                 prefix: str = ""):
+    def __init__(self, models: Models, tag: str, crud: CRUD = None, prefix: str = ""):
         super().__init__(prefix=prefix, tags=[tag], dependencies=[])
-        self.model = model
-        self.read_model = read_model
-        self.create_model = create_model
-        self.crud_model = crud_model
+        self.models = models
         self.tag = tag
         if not crud:
             # TODO proper DI
-            crud = crud_model(model=self.model, session=next(get_session()))
+            crud = models.crud(model=models.base, session=next(get_session()))
         self.crud = crud
         self.setup()
         self.extension()
@@ -48,6 +53,9 @@ class GenericRouter(APIRouter, Generic[Model, Create, Read, CRUD]):
     async def get_all_items(self, skip: int, limit: int, crud: CRUD):
         return crud.read_all(skip=skip, limit=limit)
 
+    async def update_item(self, obj_id: ObjectIdentifier, item: Update, crud: CRUD):
+        return crud.update(obj_id, **item.model_dump())
+
     async def delete_item(self, obj_id: ObjectIdentifier, crud: CRUD):
         crud.delete(obj_id)
 
@@ -55,41 +63,89 @@ class GenericRouter(APIRouter, Generic[Model, Create, Read, CRUD]):
         pass
 
     def setup(self):
-        model = self.model
-        read_model = self.read_model
-        create_model = self.create_model
+        models = self.models
         tag = self.tag
         crud = self.crud
 
-        @self.post("/", response_model=read_model, status_code=status.HTTP_201_CREATED,
-                   description=f"Creates a new {model.__name__} using {read_model.__name__}",
-                   name=f"Create a new {model.__name__}")
-        async def create_route(create_obj: create_model, session: Session = Depends(get_session),
-                               _: None = Depends(ScopeValidator(f"{tag}:create"))) -> Read:
-            return await self.create_item(create_obj, session, crud)
+        if models.create:
+            @self.post("/", response_model=models.read, status_code=status.HTTP_201_CREATED,
+                       description=f"Creates a new {models.base.__name__} using {models.read.__name__}",
+                       name=f"Create a new {models.base.__name__}")
+            async def create_route(create_obj: models.create = Depends(),
+                                   _: None = Depends(ScopeValidator(f"{tag}:create"))) -> Read:
+                return await self.create_item(create_obj, crud)
 
-        @self.get("/id/{obj_id}", response_model=read_model, status_code=status.HTTP_200_OK,
-                  description=f"Retrieves an existing {model.__name__} by its id",
-                  name=f"Retrieve a {model.__name__}")
-        async def get_route(obj_id: ObjectIdentifier = Depends(),
-                            _: None = Depends(ScopeValidator(f"{tag}:read"))):
-            obj = await self.get_item(obj_id, crud)
+        if models.read:
+            @self.get("/id/{id}", response_model=models.read, status_code=status.HTTP_200_OK,
+                      description=f"Retrieves an existing {models.base.__name__} by its id",
+                      name=f"Retrieve a {models.base.__name__}")
+            async def get_route(id: ObjectIdentifier = Depends(),
+                                _: None = Depends(ScopeValidator(f"{tag}:read"))):
+                obj = await self.get_item(id, crud)
+                if not obj:
+                    raise HTTPException(status_code=404, detail=f"{id} not found")
+                return obj
+
+        if models.read:
+            @self.get("/", response_model=list[models.read],
+                      description=f"Retrieves all existing {models.base.__name__} entities",
+                      name=f"Retrieve all {models.base.__name__} entities")
+            async def get_all_route(_: None = Depends(ScopeValidator(f"{tag}:read")),
+                                    skip: int = 0, limit: int = 10):
+                return await self.get_all_items(skip, limit, crud)
+
+        if models.update:
+            @self.put("/{id}", response_model=models.read,
+                      description=f"Updates an existing {models.base.__name__}",
+                      name=f"Update a {models.base.__name__}")
+            async def update_route(update_obj: models.update = Depends(), id: ObjectIdentifier = Depends(),
+                                   _: None = Depends(ScopeValidator(f"{tag}:update"))):
+                if not all(models.update.values()):
+                    raise HTTPException(status_code=400, detail="Not all attributes were given")
+                return await self.update_item(id, update_obj, crud)
+
+        if models.delete:
+            @self.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT,
+                         description=f"Deletes an existing {models.base.__name__}",
+                         name=f"Delete a {models.base.__name__}")
+            async def delete_route(id: models.delete = Depends(),
+                                   _: None = Depends(ScopeValidator(f"{tag}:delete"))):
+                if not crud.read(id):
+                    raise HTTPException(status_code=404, detail=f"{id} not found")
+                return await self.delete_item(id, crud)
+
+
+class UniqueNameRouter(GenericRouter[Model, Create, Read, Update, Delete, UCRUD]):
+    def __init__(self, models: Models, tag: str, crud: UCRUD = None,
+                 prefix: str = ""):
+        super().__init__(models, tag, crud, prefix)
+
+    @override
+    async def create_item(self, item: Create, crud: UCRUD):
+        # TODO only allow models with a name
+        if crud.read_by_name(item.name):
+            raise HTTPException(status_code=400, detail="Name already exists")
+        return await super().create_item(item, crud)
+
+    @override
+    def setup(self):
+        super().setup()
+        router = self
+
+        @router.get("/name/{name}", response_model=router.models.read,
+                    description=f"Retrieves an existing {router.models.base.__name__} by its name",
+                    name=f"Retrieve a {router.models.base.__name__}")
+        async def get_by_name_route(name: str, _: None = Depends(ScopeValidator(f"{router.tag}:read"))):
+            obj = router.crud.read_by_name(name)
             if not obj:
-                raise HTTPException(status_code=404, detail=f"{obj_id} not found")
+                raise HTTPException(status_code=404, detail=f"{name} not found")
             return obj
 
-        @self.get("/", response_model=list[read_model],
-                  description=f"Retrieves all existing {model.__name__} entities",
-                  name=f"Retrieve all {model.__name__} entities")
-        async def get_all_route(_: None = Depends(ScopeValidator(f"{tag}:read")),
-                                skip: int = 0, limit: int = 10):
-            return await self.get_all_items(skip, limit, crud)
-
-        @self.delete("/{obj_id}", status_code=status.HTTP_204_NO_CONTENT,
-                     description=f"Deletes an existing {model.__name__}",
-                     name=f"Delete a {model.__name__}")
-        async def delete_route(obj_id: ObjectIdentifier = Depends(),
-                               _: None = Depends(ScopeValidator(f"{tag}:delete"))):
-            if not crud.read(obj_id):
-                raise HTTPException(status_code=404, detail=f"{obj_id} not found")
-            return await self.delete_item(obj_id, crud)
+        @router.delete("/name/{name}", status_code=status.HTTP_204_NO_CONTENT,
+                       description=f"Deletes an existing {router.models.base.__name__} by its name",
+                       name=f"Delete a {router.models.base.__name__}")
+        async def delete_by_name_route(name: str, _: None = Depends(ScopeValidator(f"{router.tag}:read"))):
+            obj = router.crud.read_by_name(name)
+            if not obj:
+                raise HTTPException(status_code=404, detail=f"{name} not found")
+            router.crud.delete(ObjectIdentifier(id=obj.id))
